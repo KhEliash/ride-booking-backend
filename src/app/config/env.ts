8ea2d866486838b56e4e692362d79a20ea52b5c9
KhEliash <eliashebrahim@gmail.com>
2025-09-25 19:39:44 +0600

    role check implemented

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,10 +6,16 @@ interface EnvConfig {
   PORT: string;
   DB_URL: string;
   NODE_ENV: "development" | "production";
+  BCRYPT_SALT_ROUND: string;
+  JWT_ACCESS_EXPIRES: string;
+  JWT_ACCESS_SECRET: string;
 }
 
 export const envVars: EnvConfig = {
   PORT: process.env.PORT as string,
   DB_URL: process.env.DB_URL as string,
   NODE_ENV: process.env.NODE_ENV as "development" | "production",
+  BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+  JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
 };
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,33 +1,11 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { UserController } from "./user.controller";
-import AppError from "../../errorHelpers/AppError";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "./user.interface";
 
 const router = Router();
 
 router.post("/register", UserController.createUser);
-router.get(
-  "/all-users",
-  (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const accessToken = req.headers.authorization;
-      if (!accessToken) {
-        throw new AppError(403, "No token received");
-      }
-      const verifiedToken = jwt.verify(accessToken, "secretRide");
-      console.log(verifiedToken);
-
-      if ((verifiedToken as JwtPayload).role !== Role.ADMIN) {
-        throw new AppError(403, "You are not permitted to view this route!!");
-      }
-
-      next();
-    } catch (error) {
-      next(error);
-    }
-  },
-  UserController.getAllUsers
-);
+router.get("/all-users", checkAuth(Role.ADMIN), UserController.getAllUsers);
 
 export const UserRoutes = router;
